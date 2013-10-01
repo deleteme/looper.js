fd5c8d1491cd30df0bcf8c3d34a713df56987af2
Matt Garrett <mattga@gmail.com>
2013-09-30 23:55:56 -0700

    loop() uses recursive function instead of giant array.

--- looper.js
+++ looper.js
@@ -1,40 +1,54 @@
 /*
 Usage:
-    var sequence = [function(){}, function(){}, function(){}];
-    var loop = looper(sequence, 153);
-    loop();
+var sequence = [function(){}, function(){}, function(){}];
+var loop = looper(sequence, 153);
+loop();
 */
 var looper = function(sequence, runs){
 
-    var loop = function(value){
-        var start = Date.now();
-        var all   = [];
-        var sl    = loop.sequence.length;
-        var l     = loop.runs * sl;
-        var i     = 0;
-        var log = function(value){
-            var end = Date.now();
-            var duration = end - start;
-            var runsPerSecond = (loop.runs / duration) * 1000;
-            console.log('looper finished ' + loop.runs +
-                ' runs in ' + duration + ' ms. ' +
-                'Average ' + (runsPerSecond).toFixed(1) +
-                ' runs per second.');
-            return value;
-        };
+  var resolver     = Q.defer();
+  var promise      = resolver.promise;
+  var groupMessage = 'Looping %s runs of %s functions';
+  var logMessage   = '%s runs/s, %s functions/s';
 
-        while (i < l) {
-            all[i] = loop.sequence[i % sl];
-            i++;
-        }
-
-        return all.reduce(Q.when, Q(value)).then(log, log);
-
+  var loop = function(value){
+    console.group(groupMessage, loop.runs, loop.sequence.length);
+    console.time('Duration');
+    var start = Date.now();
+    var all   = [];
+    var sl    = loop.sequence.length;
+    var l     = loop.runs * sl;
+    var i     = 0;
+    var log = function(value){
+      var end                = Date.now();
+      var duration           = end - start;
+      var runsPerSecond      = (loop.runs / duration) * 1000;
+      var functionsPerSecond = (l / duration) * 1000;
+      console.log(logMessage, (runsPerSecond).toFixed(1), (functionsPerSecond).toFixed(1));
+      console.timeEnd('Duration');
+      console.groupEnd(groupMessage);
+      return value;
     };
 
-    loop.sequence = sequence;
-    loop.runs = runs || 25;
+    function next(value){
+      var fn = loop.sequence[i % sl];
+      if (i < l) {
+        i++;
+        return Q.fcall(fn, value).then(next);
+      } else {
+        resolver.resolve(value);
+      }
+    }
 
-    return loop;
+    next(value);
+
+    return promise.then(log, log);
+
+  };
+
+  loop.sequence = sequence;
+  loop.runs = runs || 25;
+
+  return loop;
 
 };
