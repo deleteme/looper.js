3f263e2a8e88660f0faf73ad59117420cd9085c3
Matt Garrett <mattga@gmail.com>
2017-09-09 10:27:29 -0700

    Use a bubbling event handler
    To resolve clickElement Promise faster

--- src/click-element.js
+++ src/click-element.js
@@ -1,13 +1,22 @@
 import { on, off } from './dom/event.js';
+import { $$ } from './dom/query.js'
 
-const clickElement = element => () =>
-  new Promise(resolve => {
-    const handler = () => {
-      off(element, 'click', handler);
+const clickElement = element => () => {
+  const body = () => $$('body')[0]
+  return new Promise(resolve => {
+    const directHandler = () => {
+      off(element, 'click', directHandler);
       setTimeout(resolve, 50);
     };
-    on(element, 'click', handler);
+    const bubblingHandler = e => {
+      if (e.target === element) {
+        off(body(), 'click', bubblingHandler);
+        setTimeout(resolve, 0);
+      }
+    };
+    on(body(), 'click', bubblingHandler);
+    on(element, 'click', directHandler);
     element.click();
   });
-
+};
 export default clickElement;
