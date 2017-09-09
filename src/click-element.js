210bd37207b9cb2aee29122566a131553328983e
Matt Garrett <mattga@gmail.com>
2017-09-09 10:57:57 -0700

    Direct handler will eventually unbind bubbler

--- src/click-element.js
+++ src/click-element.js
@@ -1,21 +1,27 @@
 import { on, off } from './dom/event.js';
 import { $$ } from './dom/query.js'
 
+const body = () => $$('body')[0]
+
 const clickElement = element => () => {
-  const body = () => $$('body')[0]
   return new Promise(resolve => {
+    let directHandlerTimeout;
     const directHandler = () => {
       off(element, 'click', directHandler);
-      setTimeout(resolve, 50);
+      directHandlerTimeout = setTimeout(() => {
+        off(body(), 'click', bubblingHandler);
+        resolve()
+      }, 50);
     };
     const bubblingHandler = e => {
       if (e.target === element) {
         off(body(), 'click', bubblingHandler);
+        clearTimeout(directHandlerTimeout);
         setTimeout(resolve, 0);
       }
     };
-    on(body(), 'click', bubblingHandler);
     on(element, 'click', directHandler);
+    on(body(), 'click', bubblingHandler);
     element.click();
   });
 };
