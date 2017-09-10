1a5183fb88d4db3ca1d7927accd5564b4aa18f9c
Matt Garrett <mattga@gmail.com>
2017-09-10 12:55:44 -0700

    Format with prettier, add reset button element

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sequence = [function(){}, function(){}, function(){}];
 const loop = looper(sequence, 153);
 loop();
 */
-import clickSelector from './src/click-selector.js'
+import clickSelector from './src/click-selector.js';
 import clickElement from './src/click-element.js';
 import looper from './src/looper.js';
 
--- sample-leaky-app.js
+++ sample-leaky-app.js
@@ -1,40 +1,47 @@
-import looper from "./index.js";
-import { $, $$ } from './src/dom/query.js'
+import looper from './index.js';
+import { $, $$ } from './src/dom/query.js';
 import { on } from './src/dom/event.js';
 
 // leaky app
 function make() {
-  const element = document.createElement("div");
-  element.className = "modal";
-  element.innerHTML = "modal";
+  const element = document.createElement('div');
+  element.className = 'modal';
+  element.innerHTML = 'modal';
   document.body.appendChild(element);
   return element;
 }
 
 const showModal = () => {
   hideModal();
-  make()
+  make();
 };
 
 const hideModal = () => {
-  for (let element of $$(".modal")) {
-    element.classList.add('hidden')
+  for (let element of $$('.modal')) {
+    element.classList.add('hidden');
   }
-}
+};
 
-on($("show-modal-button"), "click", showModal);
-on($("hide-modal-button"), "click", hideModal);
+on($('show-modal-button'), 'click', showModal);
+on($('hide-modal-button'), 'click', hideModal);
 
 // example looper setup
-const setup = looper.clickSelector("#show-modal-button");
-const teardown = looper.clickSelector("#hide-modal-button");
-const runs = Number($('count').value)
+const setup = looper.clickSelector('#show-modal-button');
+const teardown = looper.clickSelector('#hide-modal-button');
+const runs = Number($('count').value);
 const loop = looper([setup, teardown], runs);
 
-on($("start-loop-button"), "click", async () => {
-  $("start-loop-button").disabled = true;
-  $("log").innerHTML = "";
+on($('start-loop-button'), 'click', async () => {
+  $('start-loop-button').disabled = true;
+  $('log').innerHTML = '';
   await loop();
-  $("start-loop-button").disabled = false;
-  $("log").innerHTML = "<span style='color:red;'>Done</span>";
+  $('start-loop-button').disabled = false;
+  $('log').innerHTML = "<span style='color:red;'>Done</span>";
 });
+
+on($('reset-button'), 'click', () => {
+  for (let element of $$('.modal')) {
+    element.parentNode.removeChild(element);
+  }
+});
+
