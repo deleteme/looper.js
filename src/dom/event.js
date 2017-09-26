f0013d4e5aa78339d318e9adebbd7cc945bfa478
Matt Garrett <mattga@gmail.com>
2017-09-26 08:59:08 -0700

    remove wrap

--- src/dom/event.js
+++ src/dom/event.js
@@ -1,8 +1,5 @@
-import wrap from './wrap.js';
-export const on = wrap((element, type, handler) =>
-  element.addEventListener(type, handler)
-);
-export const off = wrap((element, type, handler) =>
-  element.removeEventListener(type, handler)
-);
+export const on = (element, type, handler) =>
+  element.addEventListener(type, handler);
+export const off = (element, type, handler) =>
+  element.removeEventListener(type, handler);
 export default { on, off };
