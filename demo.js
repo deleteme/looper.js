7a502266d4544cf9d7f5286e3843789fcb681914
Matt Garrett <mattga@gmail.com>
2017-09-26 22:35:09 -0700

    Remove polling, set loop in state

--- demo.js
+++ demo.js
@@ -3,51 +3,52 @@ import { $, $$ } from './src/dom/query.js';
 import { on } from './src/dom/event.js';
 
 const originalCountValue = Number($('count').value);
+const initialState = { isReset: true, isStarted: false, isFinished: false };
+var state = initialState;
+
+const setState = newState => {
+  state = {
+    ...state,
+    ...newState
+  };
+  render();
+};
+
+const render = () => {
+  const { isStarted, isFinished } = state;
+  const isDisabled = isStarted && !isFinished;
+  $('start-loop-button').disabled = isDisabled;
+  $('looper-loop-button').disabled = isDisabled;
+  $('log').innerHTML = isFinished ? "<span style='color:red;'>Done</span>" : '';
+};
+
+const reset = () => {
+  for (let element of $$('.modal')) {
+    element.parentNode.removeChild(element);
+  }
+  setState(initialState);
+  $('count').value = originalCountValue;
+  window.modals.length = 0;
+};
 
 // example looper setup
 on($('start-loop-button'), 'click', async () => {
+  reset();
   const setup = looper.clickSelector('#show-modal-button');
   const teardown = looper.clickSelector('#hide-modal-button');
   const runs = Number($('count').value);
   const loop = looper([setup, teardown], runs);
-  $('start-loop-button').disabled = true;
-  $('log').innerHTML = '';
-  await loop();
-  $('start-loop-button').disabled = false;
-  $('log').innerHTML = "<span style='color:red;'>Done</span>";
+  setState({ isStarted: true, isFinished: false, loop: loop() });
+  await state.loop;
+  setState({ isFinished: true });
 });
 
-on($('reset-button'), 'click', () => {
-  for (let element of $$('.modal')) {
-    element.parentNode.removeChild(element);
-  }
-  $('start-loop-button').disabled = false;
-  $('log').innerHTML = '';
-  $('count').value = originalCountValue;
-  window.modals.length = 0;
-});
+on($('reset-button'), 'click', reset);
 
 // loop the looper!
 on($('looper-loop-button'), 'click', async () => {
   await looper(
-    [
-      looper.clickSelector('#start-loop-button'),
-      () => {
-        // poll for the looping to finish
-        return new Promise(resolve => {
-          const poll = () => {
-            const isFinished = !$('start-loop-button').disabled;
-            if (isFinished) {
-              clearInterval(pollingInterval);
-              resolve();
-            }
-          };
-          let pollingInterval = setInterval(poll, 10);
-        });
-      },
-      looper.clickSelector('#reset-button')
-    ],
+    [looper.clickSelector('#start-loop-button'), () => state.loop],
     3
   )();
-  $('log').innerHTML = "<span style='color:red;'>Done</span>";
 });
